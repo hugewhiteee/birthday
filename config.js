e649757b21b90e4b5549fef2cf8641a6838860b2
hugewhiteee <69195944+hugewhiteee@users.noreply.github.com>
2020-08-04 19:12:14 +0800

    Update config.js

--- config.js
+++ config.js
@@ -30,10 +30,10 @@ var config = {
     },
     // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
     desc: {
-        turn_on: "开始了哦妈咪",
+        turn_on: "开始了哦",
         play: "音乐",
         bannar_coming: "颜色",
-        balloons_flying: "好像少点东西诶",
+        balloons_flying: "好像少点东西",
         cake_fadein: "来个蛋糕？",
         light_candle: "来根蜡烛？",
         wish_message: "生日快乐",
